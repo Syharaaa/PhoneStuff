00481989a8c6a0852fb7e0ef9a937638dfe22760
Syharaaa <50008987+Syharaaa@users.noreply.github.com>
2019-05-26 18:29:14 -0400

    Add files via upload

---
+++ Phone Code.php
@@ -0,0 +1,40 @@
+<?php
+
+interface PublicMethods{
+ 	public function callNumber($number);
+ 	public function getNumber();
+ }
+ class Phone implements PublicMethods{
+     
+    private $model;
+    private $personalNumber;
+ 	public $owner;
+ 	
+ 	public function __construct($model, $owner, $number){
+ 		$this->model = $model;
+ 		$this->owner = $owner;
+ 		if (is_int($number) && strlen($number) >= 11){
+ 		    $this->personalNumber = $number;
+ 		}elseif(!$number){
+ 		    $this->personalNumber = intval("1".rand(100000000,9999999999));
+ 		}
+ 	}
+
+ 	public function callNumber($number){
+ 		if (is_int($number) || is_string($number) && strlen($number) >= 11){
+ 			echo "Calling.. ".$number;
+ 		}else
+ 		{
+ 			echo "Unknown Number";
+ 		}
+ 	}
+ 	
+ 	public function getNumber(){
+ 	    return $this->personalNumber;
+ 	}
+
+ }
+
+ $myPhone = new Phone("Samsung Galaxy J2 Prime", "Khalid Gurmanov", NULL);
+ $myPhone->callNumber("+1(473)420-0009");
+ echo "\nMy Phone number is.. ".$myPhone->getNumber();
